bd93e59d768633afb4b8264982303324c584e0d3
aiWean <1581209979@qq.com>
2024-05-11 14:49:33 +0800

    Update fetch_deps.py

--- tools/fetch_deps.py
+++ tools/fetch_deps.py
@@ -69,7 +69,8 @@ def main():
     sys.stdout = gclient_utils.MakeFileAutoFlush(sys.stdout)
     # Make stdout annotated with the thread ids.
     sys.stdout = gclient_utils.MakeFileAnnotated(sys.stdout)
-  except (gclient_utils.Error, subprocess2.CalledProcessError) as e:
+  except (gclient_utils.Error, subprocess2.CalledProcessError), e:
+    print >> sys.stderr, 'Error: %s' % str(e)
     return 1
 
   deps_fetcher = DepsFetcher(options)
